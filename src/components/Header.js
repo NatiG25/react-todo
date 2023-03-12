4f79ff2779262d70b6701a32836b0af097e187e6
NatiG <nati.g5684@gmail.com>
2023-03-12 13:56:20 +0300

    Add & import styles into components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 import React from "react"
+import styles from "../styles/Header.module.css";
 
 const Header = () => {
+    const headerStyles = {
+        padding: '20px 0',
+        lineHeight: '1.5em',
+        color: '#aeadad',
+        textAlign: 'center',
+    }
     return (
-        <>
+        <header style={headerStyles} className={styles.header}>
             <h1>Todos</h1>
-        </>
+        </header>
     )
 }
 
